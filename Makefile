52841a41287ffb671507888017180f12fea8f801
Asko Soukka <asko.soukka@iki.fi>
2025-09-15 11:32:09 +0300

    Restore caddy proxy to publish Plone backend

--- Makefile
+++ Makefile
@@ -1,11 +1,15 @@
+.env:
+	@echo CODESPACE_NAME=$$CODESPACE_NAME >.env
+	@echo GITHUB_CODESPACES_PORT_FORWARDING_DOMAIN=$$GITHUB_CODESPACES_PORT_FORWARDING_DOMAIN >>.env
+
 .PHONY: install
-install:
+install: .env
 	@devenv shell -- echo "Ok."
 
 .PHONY: shell
-shell: ## Enter development shell
+shell: .env ## Enter development shell
 	@devenv shell
 
 .PHONY: up
-up: ## Start development services
+up: .env ## Start development services
 	@devenv up
--- devenv.nix
+++ devenv.nix
@@ -1,12 +1,32 @@
 { pkgs, config, ... }:
 {
-  package.operaton.port = 8081;
+  package.operaton.port = 8800;
   package.operaton.deployment = ./bpmn;
   processes = {
     backend.exec = "make -C backend start";
   };
 
-  process.managers.process-compose.settings.environment = [];
+  services.caddy = {
+    enable = true;
+    config = ''
+      {
+        admin off
+      }
+      :8000 {
+        ${if (config.env ? CODESPACE_NAME && config.env.CODESPACE_NAME != "") then ''
+        rewrite /VirtualHostBase/https/${config.env.CODESPACE_NAME}-8000.${config.env.GITHUB_CODESPACES_PORT_FORWARDING_DOMAIN}:443/Plone/VirtualHostRoot{uri}
+        '' else ''
+        rewrite /VirtualHostBase/http/localhost:8000/Plone/VirtualHostRoot{uri}
+        ''}
+        reverse_proxy localhost:8080
+      }
+    '';
+  };
+
+  process.managers.process-compose.settings.environment = [
+    "SERVER_USE_FORWARD_HEADERS=true"
+    "SERVER_FORWARD_HEADERS_STRATEGY=native"
+  ];
 
   tasks = {
     "bash:shell:install" = {
@@ -35,9 +55,10 @@
     export UV_PROJECT_ENVIRONMENT=$(pwd)/.venv
     export UV_PYTHON_DOWNLOADS=never
     export UV_PYTHON_PREFERENCE=system
-    export ENGINE_REST_BASE_URL=http://localhost:8081/engine-rest
+    export ENGINE_REST_BASE_URL=http://localhost:8800/engine-rest
   '';
 
   cachix.pull = [ "datakurre" ];
-  dotenv.disableHint = true;
+
+  dotenv.enable = true;
 }
